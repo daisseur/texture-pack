74965691af8591fc5145211369b0bd208e94dd6a
daisseur <daisseur@gmail.com>
2023-09-14 15:37:10 +0000

    FIXED2

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 echo "starting zip..."
-rm CustomImagesServer.zip
+rm ./CustomImagesServer.zip
 zip -j ./CustomImagesServer.zip CustomImagesServer
 sha1sum CustomImagesServer.zip > CustomImagesServer.sha1
 git add .
